a382a0ee66ae604ed529e6917c2a98d7693dfd32
Ezgi Kececi Atalay <ezgikececii@gmail.com>
2024-08-06 11:06:11 +0300

    Add files via upload

---
+++ PatikaOOP2/PatikaOOP2/Baby.cs
@@ -0,0 +1,40 @@
+﻿public class Baby
+{
+    private string _name;
+    private string _surname;
+    private DateTime _birthDate;
+
+    
+    public string Name { get { return _name; } set { _name = value; } } 
+    public string Surname { get { return _surname; } set { _surname = value; } }
+    public DateTime BirthDate { get { return _birthDate; } set { _birthDate = value; } }
+
+    public Baby()  // Default constructor
+
+    {
+        Console.WriteLine("INGAAAA!");
+        _birthDate = DateTime.Now;
+    } 
+    public Baby(string name, string surname) // Parametre ile çalışacak constructor
+    {
+        _name = name;
+        _surname = surname;
+        Console.WriteLine("INGAAAA!");
+        _birthDate = DateTime.Now;
+    }
+
+
+    public void NewBornBaby() 
+    {
+        
+        Console.WriteLine($"{_name} {_surname} adlı bebek {_birthDate} tarihinde ve saatinde doğdu.");
+
+    }
+
+
+
+}
+
+ 
+
+    
---
+++ PatikaOOP2/PatikaOOP2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection.Metadata;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Baby baby1 = new Baby("Minik", "Bebek"); //Baby sınıfından parametre alan constructor ile yeni nesne
+        baby1.NewBornBaby();
+        Console.WriteLine("----------------------");
+
+        Baby baby2 = new Baby(); //Baby sınıfından default constructor ile yeni nesne
+        baby2.Name = "Küçük";
+        baby2.Surname = "Bebek";
+        baby2.NewBornBaby();
+    }
+    
+}
